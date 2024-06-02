26015e27b621770dff17edd704a344a7cf062f56
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-06-02 15:56:51 -0300

    🔨 feat: implements Criar method of TarefaController
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -81,7 +81,9 @@ public class TarefaController : ControllerBase
         if (tarefa.Data == DateTime.MinValue)
             return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-        // TODO: Adicionar a tarefa recebida no EF e salvar as mudanças (save changes)
+        _context.Add(tarefa);
+        _context.SaveChanges();
+
         return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
     }
 
