5559e3e60d8af94c02143482aaf4ab34a4205d47
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-06-02 15:55:50 -0300

    🔨 feat: implements ObterPorStatus method of TarefaController
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -65,10 +65,14 @@ public class TarefaController : ControllerBase
     [HttpGet("ObterPorStatus")]
     public IActionResult ObterPorStatus(EnumStatusTarefa status)
     {
-        // TODO: Buscar  as tarefas no banco utilizando o EF, que contenha o status recebido por parâmetro
-        // Dica: Usar como exemplo o endpoint ObterPorData
-        var tarefa = _context.Tarefas.Where(x => x.Status == status);
-        return Ok(tarefa);
+        var tarefas = _context.Tarefas.Where(x => x.Status == status);
+
+        if (!tarefas.Any())
+        {
+            return NotFound();
+        }
+
+        return Ok(tarefas);
     }
 
     [HttpPost]
